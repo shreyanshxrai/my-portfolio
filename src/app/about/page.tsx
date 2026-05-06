ffbf7bb11dfbb9ac5e82afd7008e4108d2633981
Shreyansh Rai <shreyanshtalks@gmail.com>
2026-05-06 11:46:45 +0530

    created basic pages : about and projects

---
+++ src/app/about/page.tsx
@@ -0,0 +1,4 @@
+export default function About(){
+    return(<>
+    <div className="p-10 text-3xl font-bold">About page</div>
+    </>)}
---
+++ src/app/projects/page.tsx
@@ -0,0 +1,5 @@
+export default function Projects(){
+    return(<>
+    <div className="p-10 text-3xl font-bold">Projects page</div>
+    
+    </>)}
